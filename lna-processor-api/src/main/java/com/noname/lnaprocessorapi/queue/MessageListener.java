4b87a0297b9c0380211cf1540e1777a48e646bf2
Piotras <poleszak@protonmail.com>
2025-03-09 16:04:38 +0100

    Changes

--- lna-processor-api/src/main/java/com/noname/lnaprocessorapi/queue/MessageListener.java
+++ lna-processor-api/src/main/java/com/noname/lnaprocessorapi/queue/MessageListener.java
@@ -5,18 +5,11 @@ import com.noname.lnaprocessordto.MessageRequestDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-import java.util.logging.Logger;
-
 @Service
 public class MessageListener {
 
-
-    @RabbitListener(queues = "queue_1")  // same queue name as in your definitions.json
+    @RabbitListener(queues = "queue_1")
     public void receiveMessage(MessageRequestDTO messageRequestDTO) {
         System.out.println(messageRequestDTO.getMessage());
-        // Or simply do:
-        // System.out.println("Received: " + messageRequestDTO);
-
-        // Add your logic here, e.g., store in DB, call another service, etc.
     }
 }
